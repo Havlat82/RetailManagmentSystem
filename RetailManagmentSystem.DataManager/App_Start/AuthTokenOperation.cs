538579318f2e6d678527523ee5813186111ed26b
Petr Havlát <havlat82@gmail.com>
2024-04-13 21:13:12 +0200

    Modified AuthTokenOperation class to follow single responsibility principle

--- RetailManagmentSystem.DataManager/App_Start/AuthTokenOperation.cs
+++ RetailManagmentSystem.DataManager/App_Start/AuthTokenOperation.cs
@@ -10,39 +10,40 @@ namespace RetailManagmentSystem.DataManager.App_Start
     {
         public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
         {
-            swaggerDoc.paths.Add("/token", new PathItem
+            var tags = new List<string> { "Auth" };
+            var consumes = new List<string> { "application/x-www-form-urlencoded" };
+
+            var parameters = new List<Parameter> {
+                CreateParameter("grant_type", "string", "formData", "password", true),
+                CreateParameter("username", "string", "formData", null, false),
+                CreateParameter("password", "string", "formData", null, false)
+            };
+
+            swaggerDoc.paths.Add("/token", new PathItem { post = CreatePostOperation(tags, consumes, parameters) });
+        }
+
+        private Operation CreatePostOperation(List<string> tags, List<string> consumes, List<Parameter> parameters)
+        {
+            var o = new Operation
             {
-                post = new Operation
-                {
-                    tags = new List<string> { "Auth" },
-                    consumes = new List<string> { "application/x-www-form-urlencoded" },
-                    parameters = new List<Parameter>
-                    {
-                        new Parameter
-                        {
-                            type = "string",
-                            name = "grant_type",
-                            required = true,
-                            @in = "formData",
-                            @default = "password"
-                        },
-                        new Parameter
-                        {
-                            type = "string",
-                            name = "username",
-                            required = false,
-                            @in = "formData"
-                        },
-                        new Parameter
-                        {
-                            type = "string",
-                            name = "password",
-                            required = false,
-                            @in = "formData"
-                        }
-                    }
-                }
-            });
+                tags = tags,
+                consumes = consumes,
+                parameters = parameters
+            };
+            return o;
+        }
+
+        private Parameter CreateParameter(string name, string type, string @in, string @default, bool required)
+        {
+            var p = new Parameter
+            {
+                type = type,
+                name = name,
+                required = required,
+                @in = @in,
+                @default = @default
+            };
+            return p;
         }
     }
 }
